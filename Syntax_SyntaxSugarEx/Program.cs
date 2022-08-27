5ea27dcd126d7dd1a9b24dced6116d102ffe12b5
KyleCSharp <kylelrigler@gmail.com>
2022-08-27 13:09:41 -0400

    first commit

---
+++ Syntax_SyntaxSugarEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Syntax_SyntaxSugarEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less then nine" : $"{answer}is greater the nine";
+            Console.WriteLine(response);
+
+            //or for interaction
+
+            var typeMe = int.Parse(Console.ReadLine());
+            var responcex = (typeMe < 9) ? $"{typeMe} is less then nine" : $"{typeMe} is greater then nine";
+            Console.WriteLine(responcex);
+            
+        }
+    }
+}
